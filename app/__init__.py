cc29939111359751b5c91e4e7f8d04d6feff135b
Donny van der Meer <dvm@sanger.ac.uk>
2017-12-16 18:08:38 +0000

    Add flask bootstrap to pages and forms

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -11,6 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(Config)
 
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
